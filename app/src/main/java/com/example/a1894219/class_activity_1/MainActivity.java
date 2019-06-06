bddf6c532db5050f26fa561641bf69e19e6f1d57
Kakadiya <1894219@cegepgim.lcl>
2019-06-06 14:41:06 -0400

    Initial commit

---
+++ app/src/main/java/com/example/a1894219/class_activity_1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a1894219.class_activity_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
